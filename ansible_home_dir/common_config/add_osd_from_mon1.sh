a5a9fa9d9e0746e2a402229e9c4d2d2bdbc3d55d
root <root@artifactory.idm.mci.ir>
2023-05-24 15:49:38 +0330

    secondVersion

--- ansible_home_dir/common_config/add_osd_from_mon1.sh
+++ ansible_home_dir/common_config/add_osd_from_mon1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in {1..8}; do ceph orch daemon add osd kceph3-15:/dev/ceph-block-$i/block$i; sleep 20; done
+for i in {1..8}; do ceph orch daemon add osd ceph2-15:/dev/ceph-block-$i/block$i; sleep 20; done
